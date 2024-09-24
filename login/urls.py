7d50d5468f1e8da7721db655140e306fe2c194ae
evelina <evelina.n.05@mail.ru>
2024-09-24 10:14:52 +0300

    Добавили регистрацию и отредактировали проект

--- login/urls.py
+++ login/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('login', views.login_view)
+    path('login/', views.login_view),
+    path('register/', views.register_view)
 ]
--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login
 from django.http import HttpRequest, HttpResponse
+from . import forms
 
 def login_view(request:HttpRequest) -> HttpResponse:
     if request.method=='POST':
@@ -15,3 +16,21 @@ def login_view(request:HttpRequest) -> HttpResponse:
             return redirect('/')
         
     return render(request, 'login/login.html')
+
+def register_view(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+        form = forms.RegisterForm()
+        return  render(request, 'login/register.html', {
+            'form': form
+        })
+    if request.method == 'POST':
+        form = forms.RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return  redirect('/')
+        else:
+            return render(request, 'login/register.html', {
+                'form': form
+            })
